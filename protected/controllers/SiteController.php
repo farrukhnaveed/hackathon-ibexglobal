9e8e75f655c3f2d4c0dbbf055bbc8a18a761eb66
zeeshan_shakil <zshan.shakil@gmail.com>
2017-12-25 17:04:07 +0500

    Data Displayed

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -3,63 +3,76 @@
 /**
  * SiteController is the default controller to handle user requests.
  */
-class SiteController extends CController
-{
-	/**
-	 * Index action is the default action in a controller.
-	 */
-	public function actionIndex()
-	{
-		$is_result = false;
-		$errors = array();
-		$data = array();
-		if(Yii::app()->request->isPostRequest) {
-			if(isset( $_FILES) && isset($_FILES["csv1"]) && isset($_FILES["csv2"]) && isset($_FILES["csv3"])) {
-				$csv_model = new CSVModel();
-				$validation_result1 = $csv_model->validateFileFormat($_FILES["csv1"]);
-				$validation_result2 = $csv_model->validateFileFormat($_FILES["csv2"]);
-				$validation_result3 = $csv_model->validateFileFormat($_FILES["csv3"]);
+class SiteController extends CController {
 
-				if ($validation_result1) {
-				//if ($validation_result1 && $validation_result2 && $validation_result3) {
-					$content1 = $csv_model->getCSVFileContent($_FILES["csv1"]);
-					$content2 = $csv_model->getCSVFileContent($_FILES["csv2"]);
-					$content3 = $csv_model->getCSVFileContent($_FILES["csv3"]);
+    /**
+     * Index action is the default action in a controller.
+     */
+    public function actionIndex() {
+        $is_result = false;
+        $errors = array();
+        $data = array();
+        if (Yii::app()->request->isPostRequest) {
+            if (isset($_FILES) && isset($_FILES["csv1"]) && isset($_FILES["csv2"]) && isset($_FILES["csv3"])) {
+                $csv_model = new CSVModel();
+                $validation_result1 = $csv_model->validateFileFormat($_FILES["csv1"]);
+                $validation_result2 = $csv_model->validateFileFormat($_FILES["csv2"]);
+                $validation_result3 = $csv_model->validateFileFormat($_FILES["csv3"]);
 
-					if ($content1 == false) {
-						array_push($errors, 'File 1 content invalid');
-					}
-					if ($content2 == false) {
-						array_push($errors, 'File 2 content invalid');
-					}
-					if ($content3 == false) {
-						array_push($errors, 'File 3 content invalid');
-					}
+                if ($validation_result1) {
+                    //if ($validation_result1 && $validation_result2 && $validation_result3) {
+                    $content1 = $csv_model->getCSVFileContent($_FILES["csv1"]);
+                    $content2 = $csv_model->getCSVFileContent($_FILES["csv2"]);
+                    $content3 = $csv_model->getCSVFileContent($_FILES["csv3"]);
 
-					if (count($errors) == 0) {
-						//get results
-						$data = $csv_model->getEmployeeAggregate($content1,$content2,$content3);
-						$is_result = true;
-					}
-				} else {
-					if ($validation_result1 == false) {
-					array_push($errors, 'File 1 is not a valid CSV file');
-					}
-					if ($validation_result2 == false) {
-						array_push($errors, 'File 2 is not a valid CSV file');
-					}
-					if ($validation_result3 == false) {
-						array_push($errors, 'File 3 is not a valid CSV file');
-					}	
-				}
-									
-			} else {
-				array_push($errors, 'One or more files are missing');
-			}
-		}
+                    if ($content1 == false) {
+                        array_push($errors, 'File 1 content invalid');
+                    }
+                    if ($content2 == false) {
+                        array_push($errors, 'File 2 content invalid');
+                    }
+                    if ($content3 == false) {
+                        array_push($errors, 'File 3 content invalid');
+                    }
 
-		$this->layout='/layouts/default';
-    	$this->render('index',array('isResult'=>$is_result,'errors'=>$errors,'data'=>$data));
-	}
+                    if (count($errors) == 0) {
+                        //get results
+                        $data = $csv_model->getEmployeeAggregate($content1, $content2, $content3);
+                        $is_result = true;
+                    }
+                } else {
+                    if ($validation_result1 == false) {
+                        array_push($errors, 'File 1 is not a valid CSV file');
+                    }
+                    if ($validation_result2 == false) {
+                        array_push($errors, 'File 2 is not a valid CSV file');
+                    }
+                    if ($validation_result3 == false) {
+                        array_push($errors, 'File 3 is not a valid CSV file');
+                    }
+                }
+            } else {
+                array_push($errors, 'One or more files are missing');
+            }
+        }
+        
+        Yii::app()->session['data'] = $data;
+        $this->layout = '/layouts/default';
+        $this->render('index', array('isResult' => $is_result, 'errors' => $errors, 'data' => $data));
+    }
+
+    public function actionDownload() {
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=output.csv");
+        header("Content-Type: application/octet-stream");
+        header("Content-Transfer-Encoding: binary");
+        $out = "EmployeeID,Name,HoursWorked\r\n";
+        $headCount = 1;
+        foreach (Yii::app()->session['data'] as $key=>$val) {
+            $out .= $key.",".implode(",", $val) . "\r\n";
+        }
+        echo $out;
+    }
 
 }
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,80 +1,125 @@
 
 <aside id="fh5co-hero">
-	<div class="flexslider">
-		<ul class="slides">
-	   	<li style="background-image: url('<?php echo Yii::app()->request->baseUrl?>/static/images/img_bg_3.jpg');">
-	   		<div class="overlay-gradient"></div>
-	   		<div class="container-fluids">
-	   			<div class="row">
-		   			<div class="col-md-6 col-md-offset-3 slider-text slider-text-bg">
-		   				<div class="slider-text-inner text-center">
-		   					<h1>Upload CSV</h1>
-								<h2>Upload csv and get Employee Aggregate work hours</h2>
-		   				</div>
-		   			</div>
-		   		</div>
-	   		</div>
-	   	</li>		   	
-	  	</ul>
-  	</div>
+    <div class="flexslider">
+        <ul class="slides">
+            <li style="background-image: url('<?php echo Yii::app()->request->baseUrl ?>/static/images/img_bg_3.jpg');">
+                <div class="overlay-gradient"></div>
+                <div class="container-fluids">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3 slider-text slider-text-bg">
+                            <div class="slider-text-inner text-center">
+                                <h1>Upload CSV</h1>
+                                <h2>Upload csv and get Employee Aggregate work hours</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>		   	
+        </ul>
+    </div>
 </aside>		
 <div id="fh5co-contact">
-	<div class="row animate-box">
-		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
-			<h2>CSV file rules</h2>
-			<p>Consolidate all the three csv based on EmployeeId and aggregate the “hours worked” field for common EmployeeId (across files or within each file).  Where the EmployeeId only shows up once then you need to mention the above three fields for the EmployeeId as well. Each EmployeeId will only have one row in the final csv file that your program will produce.</p>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-3 col-md-push-1 animate-box">
-			<h3>Creater</h3>
-			<ul class="contact-info">
-				<li><i class="icon-user"></i><a href="mailto:farrukh.naveed@ibexglobal.com">Farrukh Naveed</a></li>
-				<li><i class="icon-user"></i><a href="mailto:zeeshan.shakil@ibexglobal.com">Zeeshan Shakil</a></li>
-			</ul>
-		</div>
-		<div class="col-md-7 col-md-push-1 animate-box">
-			<div class="row">
-				<form action="" method="post" enctype="multipart/form-data">
-					<div class="file-input">
-						<div class="col-md-2">
-							<div class="form-group">
-								<p>CSV File 1</p>
-							</div>
-						</div>
-						<div class="col-md-10">
-							<div class="form-group">
-								<input required="true" name="csv1" type="file" class="form-control" accept=".csv">
-							</div>
-						</div>
-						<div class="col-md-2">
-							<div class="form-group">
-								<p>CSV File 2</p>
-							</div>
-						</div>
-						<div class="col-md-10">
-							<div class="form-group">
-								<input required="true" name="csv2" type="file" class="form-control" accept=".csv">
-							</div>
-						</div>
-						<div class="col-md-2">
-							<div class="form-group">
-								<p>CSV File 3</p>
-							</div>
-						</div>
-						<div class="col-md-10">
-							<div class="form-group">
-								<input required="true" name="csv3" type="file" class="form-control" accept=".csv">
-							</div>
-						</div>
-					</div>
-					<div class="col-md-12">
-						<div class="form-group">
-							<input type="submit" value="Get Result" class="btn btn-primary btn-modify">
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+    <div class="row animate-box">
+        <div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+            <h2>CSV file rules</h2>
+            <p>Consolidate all the three csv based on EmployeeId and aggregate the “hours worked” field for common EmployeeId (across files or within each file).  Where the EmployeeId only shows up once then you need to mention the above three fields for the EmployeeId as well. Each EmployeeId will only have one row in the final csv file that your program will produce.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-md-push-1 animate-box">
+            <h3>Creater</h3>
+            <ul class="contact-info">
+                <li><i class="icon-user"></i><a href="mailto:farrukh.naveed@ibexglobal.com">Farrukh Naveed</a></li>
+                <li><i class="icon-user"></i><a href="mailto:zeeshan.shakil@ibexglobal.com">Zeeshan Shakil</a></li>
+            </ul>
+        </div>
+        <div class="col-md-7 col-md-push-1 animate-box">
+            <!--            <div class="alert alert-danger alert-dismissible fade in" role="alert"> 
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button> 
+                            <strong> OOPS!!</strong> <br>
+                            
+                        </div>-->
+            <?php if(count($errors) > 0){ ?>
+                <ul>
+                <?php foreach ($errors as $error) { ?>
+                    <li  class="text-danger"><?= $error; ?></li>
+                <?php }  ?>
+                </ul>
+                <?php }  ?>
+            <div class="row">
+                <?php
+                if ($isResult == false) {
+                    ?>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="file-input">
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <p>CSV File 1</p>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <div class="form-group">
+                                    <input required="true" name="csv1" type="file" class="form-control" accept=".csv">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <p>CSV File 2</p>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <div class="form-group">
+                                    <input required="true" name="csv2" type="file" class="form-control" accept=".csv">
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="form-group">
+                                    <p>CSV File 3</p>
+                                </div>
+                            </div>
+                            <div class="col-md-10">
+                                <div class="form-group">
+                                    <input required="true" name="csv3" type="file" class="form-control" accept=".csv">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="submit" value="Get Result" class="btn btn-primary btn-modify">
+                            </div>
+                        </div>
+                    </form>
+                <?php } else { ?>
+                <a class="btn-sm btn-primary col-md-offset-9" target="_blank" href="Site/Download">
+                                        Download File
+                </a><br><br>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <td>Employee ID</td>
+                                <td>Name</td>
+                                <td>Hours Worked</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($data as $key=>$val) { ?>
+                                <tr>
+                                    <td><?= $key ?></td>
+                                    <td><?= $val['Name'] ?></td>
+                                    <td><?= $val['HoursWorked'] ?></td>
+                                </tr>
+                            <?php }  ?>
+                            
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="3">
+                                    </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 </div>
