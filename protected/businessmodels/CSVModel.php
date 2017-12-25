4003b3cd8e6f26b703a41c4210d58d81551692b5
Farrukh Naveed <farrukh.naveed@ibexglobal.com>
2017-12-25 16:29:46 +0500

    backend complete

--- protected/businessmodels/CSVModel.php
+++ protected/businessmodels/CSVModel.php
@@ -2,27 +2,99 @@
 
 class CSVModel {
 
-	public function getCSVFileContent($csv_file){
-		if ($this->validateCSV($csv_file)) {
-			$mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
-			if(in_array($csv_file['type'],$mimes)){
-				$content = file_get_contents($csv_file["tmp_name"]);
-				return $content;
-			} else {
-				return false;
-			}
+	public function validateFileFormat($csv_file) {
+		$mimes = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+
+		if (in_array($csv_file['type'],$mimes)) {
+			$result = true;
 		} else {
-			return false;
+			$result = false;
 		}
+
+		return $result;
 	}
 
-	public function validateCSV($csv_file) {
-		//TODO; validate csv
-		return true;
+	public function getCSVFileContent($csv_file){
+		$content = file_get_contents($csv_file["tmp_name"]);
+		$result = $this->validateContent($content);
+
+		if ($result == true) {
+			$result = $content;
+		}
+
+		return $content;
 	}
+
+	public function validateContent($content) {
+		$rows = explode("\n",$content);
+		$title = explode(",",$rows[0]);
+
+		foreach ($title as $key => $value) {
+			$title[$key] = trim(strtolower($title[$key]));
+		}
+
+		if (in_array('employeeid', $title) && in_array('hoursworked', $title)) {
+			$result = true;
+		} else {
+			$result = false;
+		}
+
+		return $result;
+	}
+
 
 	public function getEmployeeAggregate($content1,$content2,$content3) {
-		$result = array();
-		return $result;
+		$data = array();
+
+		for($i=1;$i<=3;$i++) {
+			$id_index = $name_index = $hours_index = -1;
+
+			if ($i==1) {
+				$content = $content1;
+			} else if ($i==2) {
+				$content = $content2;
+			} else {
+				$content = $content3;
+			}
+
+			$rows = explode("\n",$content);
+			$title = explode(",",$rows[0]);
+
+			foreach ($title as $key => $value) {
+				$title[$key] = trim(strtolower($title[$key]));
+
+				if ($title[$key] == 'employeeid') {
+					$id_index = $key;
+				} else if ($title[$key] == 'hoursworked') {
+					$hours_index = $key;
+				} elseif ($title[$key] == 'name') {
+					$name_index = $key;
+				}
+			}
+
+			$content_first = true;
+			foreach ($rows as $row) {
+				if ($content_first) {
+					$content_first = false;
+				} else {
+					$row_data = explode(",",$row);
+
+					if ($row_data[$id_index] != '' && $row_data[$name_index] != '' && $row_data[$hours_index] != '') {
+						if (isset($data[trim($row_data[$id_index])])) {
+
+							$data[trim($row_data[$id_index])]['HoursWorked'] += intval(trim($row_data[$hours_index]));
+						} else {
+							//print_r($data);
+							$data[trim($row_data[$id_index])] = array('Name'=>$row_data[$name_index],'HoursWorked'=>intval(trim($row_data[$hours_index])));
+
+							//print_r($data);
+						}
+					}
+						
+				}
+			}
+		}
+		
+		return $data;
 	}
 }
--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -16,26 +16,48 @@ class SiteController extends CController
 		if(Yii::app()->request->isPostRequest) {
 			if(isset( $_FILES) && isset($_FILES["csv1"]) && isset($_FILES["csv2"]) && isset($_FILES["csv3"])) {
 				$csv_model = new CSVModel();
-				$content1 = $csv_model->getCSVFileContent($_FILES["csv1"]);
-				$content2 = $csv_model->getCSVFileContent($_FILES["csv2"]);
-				$content3 = $csv_model->getCSVFileContent($_FILES["csv3"]);
+				$validation_result1 = $csv_model->validateFileFormat($_FILES["csv1"]);
+				$validation_result2 = $csv_model->validateFileFormat($_FILES["csv2"]);
+				$validation_result3 = $csv_model->validateFileFormat($_FILES["csv3"]);
 
-				if ($content1 == false) {
-					array_push($errors, 'File 1 is invalid');
+				if ($validation_result1) {
+				//if ($validation_result1 && $validation_result2 && $validation_result3) {
+					$content1 = $csv_model->getCSVFileContent($_FILES["csv1"]);
+					$content2 = $csv_model->getCSVFileContent($_FILES["csv2"]);
+					$content3 = $csv_model->getCSVFileContent($_FILES["csv3"]);
+
+					if ($content1 == false) {
+						array_push($errors, 'File 1 content invalid');
+					}
+					if ($content2 == false) {
+						array_push($errors, 'File 2 content invalid');
+					}
+					if ($content3 == false) {
+						array_push($errors, 'File 3 content invalid');
+					}
+
+					if (count($errors) == 0) {
+						//get results
+						$data = $csv_model->getEmployeeAggregate($content1,$content2,$content3);
+						$is_result = true;
+					}
+				} else {
+					if ($validation_result1 == false) {
+					array_push($errors, 'File 1 is not a valid CSV file');
+					}
+					if ($validation_result2 == false) {
+						array_push($errors, 'File 2 is not a valid CSV file');
+					}
+					if ($validation_result3 == false) {
+						array_push($errors, 'File 3 is not a valid CSV file');
+					}	
 				}
-				if ($content2 == false) {
-					array_push($errors, 'File 2 is invalid');
-				}
-				if ($content3 == false) {
-					array_push($errors, 'File 3 is invalid');
-				}
-				if (count($errors) == 0) {
-					//get results
-					$is_result = true;
-					$data = $csv_model->getEmployeeAggregate($content1,$content2,$content3);
-				}
+									
+			} else {
+				array_push($errors, 'One or more files are missing');
 			}
 		}
+
 		$this->layout='/layouts/default';
     	$this->render('index',array('isResult'=>$is_result,'errors'=>$errors,'data'=>$data));
 	}
